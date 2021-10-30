9c8e0a66cad5eb88fa9c96d7b5cfec10678b4446
Yllka Uka <yu47769@ubt-uni.net>
2021-10-31 01:58:38 +0200

    End of Section - 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -40,6 +40,12 @@ namespace API
 {
             opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
     });
+    services.AddCors(otp =>
+    {
+        otp.AddPolicy("CorsPolicy", policy => {
+            policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+        });
+    });
 }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -56,6 +62,7 @@ namespace API
 
             app.UseRouting();
 
+            app.UseCors ("CorsPolicy");
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
